0988df29a4d596312ca6aa0d3eecdbaee3ac28fe
pigneedle <drascus1@gmail.com>
2016-12-14 14:14:04 -0700

    First commit

--- com_dndcharactersheets/admin/sql/unistall.mysql.utf8.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `#__dndcs_character`;
-DROP TABLE IF EXISTS `#__dndcs_sheet`;
-DROP TABLE IF EXISTS `#__dndcs_description`;
-DROP TABLE IF EXISTS `#__dndcs_core_stat_list`;
-DROP TABLE IF EXISTS `#__dndcs_core_stat`;
-DROP TABLE IF EXISTS `#__dndcs_skill_list`;
-DROP TABLE IF EXISTS `#__dndcs_skill`;
---
+++ com_dndcharactersheets/admin/views/dndcharactersheets/view.html.php
@@ -0,0 +1,45 @@
+<?php defined('_JEXEC') or die('Restricted access');
+/**
+ * General Controller of DnDCharacterSheets component
+ *
+ * @package     Dwarfholm-DnD
+ * @subpackage  com_dndcharactersheets
+ * @since       0.0.2
+ * 
+ * @copyright   Copyright (C) 2016 Dwarfholm.com. All rights reserved.
+ */
+ 
+/**
+ * CharacterSheetsSettings View
+ *
+ * @since  0.0.2
+ */
+jimport('joomla.application.component.view');
+
+class DnDCharacterSheetsViewCharacterSheetsSettings extends JViewLegacy
+{
+	/**
+	 * Display the Character Sheet Settings view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Get data from the model
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+ 
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+ 
+			return false;
+		}
+ 
+		// Display the template
+		parent::display($tpl);
+	}
+}
